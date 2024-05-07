43ef9d841db8d148410c3f03477cf22aa212bc29
Houn Ravy <hounravy605@gmail.com>
2024-05-07 15:17:13 +0700

    mu up

--- app.js
+++ app.js
@@ -119,3 +119,15 @@ bot.action('back_to_main', (ctx) => {
 });
 
 bot.launch();
+
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello world!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log("Server is running on port 3000"));
